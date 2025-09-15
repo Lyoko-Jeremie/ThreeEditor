27bdc3c4a1857a2a4867eea14dc35390bfb36df9
LyokoJeremie <lucheng989898@gmail.com>
2025-09-15 19:01:40 +0800

    initializeReteEditor

--- rete-editor/src/main.ts
+++ rete-editor/src/main.ts
@@ -7,7 +7,7 @@ import {
 import {LitPlugin, Presets, type LitArea2D} from '@retejs/lit-plugin';
 import {type MinimapExtra, MinimapPlugin} from "rete-minimap-plugin";
 import {HistoryPlugin, type HistoryActions, Presets as PresetsHistory, HistoryExtensions} from "rete-history-plugin";
-import {CommentPlugin, CommentExtensions} from "rete-comment-plugin";
+// import {CommentPlugin, CommentExtensions} from "rete-comment-plugin";
 import {type ContextMenuExtra, ContextMenuPlugin, Presets as ContextMenuPresets} from "rete-context-menu-plugin";
 import {DataflowEngine} from "rete-engine";
 import Swal from 'sweetalert2';
@@ -16,7 +16,7 @@ import Swal from 'sweetalert2';
 
 class Node extends ClassicPreset.Node {
 	width = 200;
-	height = 120;
+	height!: number;
 
 	constructor(label: string, id: string) {
 		super(label);
@@ -60,29 +60,28 @@ async function initializeReteEditor(container: HTMLElement) {
 		accumulating: AreaExtensions.accumulateOnCtrl(),
 	});
 
-	// @ts-ignore
 	const minimap = new MinimapPlugin<Schemes>();
 
 	const history = new HistoryPlugin<Schemes, HistoryActions<Schemes>>();
 
-	const comment = new CommentPlugin<Schemes, AreaExtra>({
-		edit: async (comment) => {
-			const os = comment.text;
-			return Swal.fire({
-				title: 'Edit comment',
-				input: 'text',
-				inputValue: os,
-				showCancelButton: true,
-				confirmButtonText: 'Save',
-				cancelButtonText: 'Cancel',
-			}).then((result) => {
-				if (result.isConfirmed) {
-					return result.value || os;
-				}
-				return os;
-			});
-		}
-	});
+	// const comment = new CommentPlugin<Schemes, AreaExtra>({
+	// 	edit: async (comment) => {
+	// 		const os = comment.text;
+	// 		return Swal.fire({
+	// 			title: 'Edit comment',
+	// 			input: 'text',
+	// 			inputValue: os,
+	// 			showCancelButton: true,
+	// 			confirmButtonText: 'Save',
+	// 			cancelButtonText: 'Cancel',
+	// 		}).then((result) => {
+	// 			if (result.isConfirmed) {
+	// 				return result.value || os;
+	// 			}
+	// 			return os;
+	// 		});
+	// 	}
+	// });
 
 	const contextMenu = new ContextMenuPlugin<Schemes>({
 		items: ContextMenuPresets.classic.setup([
@@ -122,38 +121,74 @@ async function initializeReteEditor(container: HTMLElement) {
 	area.use(render);
 	area.use(minimap);
 	area.use(history);
-	area.use(comment);
+	// area.use(comment);
 	area.use(contextMenu);
 
 	const selector = AreaExtensions.selector();
 	const accumulating = AreaExtensions.accumulateOnCtrl();
 
-	CommentExtensions.selectable(comment, selector, accumulating);
+	// CommentExtensions.selectable(comment, selector, accumulating);
 
 	AreaExtensions.simpleNodesOrder(area);
 
 	const a = new Node("A", 'xxxx-node-id-aaa');
-	// a.id = 'xxxx-node-id-aaa';
 	a.addControl("a", new ClassicPreset.InputControl("text", {initial: "a"}));
 	a.addOutput("a", new ClassicPreset.Output(socket));
 	await editor.addNode(a);
 
 	const b = new Node("B", 'xxxx-node-id-bbb');
-	// a.id = 'xxxx-node-id-bbb';
 	b.addControl("b", new ClassicPreset.InputControl("text", {initial: "b"}));
 	b.addInput("b", new ClassicPreset.Input(socket));
 	await editor.addNode(b);
 
-	comment.addInline("Inline comment text", [100, -20], b.id);
-	comment.addFrame("Frame comment text", [a.id]);
+	// comment.addInline("Inline comment text", [100, -20], b.id);
+	// comment.addFrame("Frame comment text", [a.id]);
+
+	const c = new Node("C", 'xxxx-node-id-ccc');
+	c.addInput("c", new ClassicPreset.Input(socket, 'c', true));
+	c.addOutput("c", new ClassicPreset.Output(socket));
+	c.addControl("c", new ClassicPreset.InputControl("text", {initial: "c"}));
+	await editor.addNode(c);
 
 	await editor.addConnection(new ClassicPreset.Connection(a, "a", b, "b"));
 
 
 	await area.translate(a.id, {x: 0, y: 0});
 	await area.translate(b.id, {x: 270, y: 0});
+	await area.translate(c.id, {x: -220, y: 0});
 
 	setTimeout(() => {
+
+		editor.getNodes().forEach(T => {
+			T.height
+		});
+		console.log('editor.getNodes()', editor.getNodes());
+		console.log('editor', editor);
+		console.log('area', area);
+		const viewsElements = ((area as any).elements as {
+			viewsElements: Map<string, HTMLDivElement>,
+			views: WeakMap<HTMLDivElement, { type: string, element: HTMLElement, payload: any }>
+		}).viewsElements;
+		console.log('viewsElements', viewsElements);
+		for (const [id, el] of viewsElements) {
+			console.log('el', id, el, el.clientHeight);
+			if (id.startsWith('node_')) {
+				const nodeId = id.replace('node_', '');
+				const node = editor.getNodes().find(n => n.id === nodeId);
+				if (node) {
+					console.log('node', node, node.height);
+					node.height = el.clientHeight;
+					console.log('node.height', node.height);
+				}
+				continue;
+			}
+		}
+		// area.update();
+		console.log('minimap', minimap);
+		(minimap as any).render();
+
+		// console.log('comment', comment);
+
 		// wait until nodes rendered because they dont have predefined width and height
 		AreaExtensions.zoomAt(area, editor.getNodes());
 	}, 100);
